1a4df69fd315d9ddb22c1ea3a8778a9daca29a3e
zhuoyu <zhuoyu.yong.2024@computing.smu.edu.sg>
2026-05-25 13:48:02 +0800

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # #brute force method: try every possible pair and see if they add up to the sum 
+        # for i in range(len(nums)):
+        #     #inner loop starts at the second number
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             return [i, j]
+        #create a hash map 
+        seen = {}
+        #iterate through the array
+        for i in range(len(nums)):
+            #calculate the complement of each num 
+            complement = target - nums[i]
+            #check if the complement is the hashmap 
+            if complement not in seen:
+                #if not, store the number as a key and its index as a value
+                seen[nums[i]] = i 
+            #if the complement is in hashmap, return the index of that complement and current index
+            else:
+                return [seen[complement], i]
+        return []
