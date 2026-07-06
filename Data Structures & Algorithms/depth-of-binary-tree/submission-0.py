d339604ed51afa7bee01a4747bff79e2403de21b
zhuoyu <yongzhuoyu@gmail.com>
2026-07-06 12:00:35 +0800

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Base Case: Depth of an empty tree should be None 
+        if root is None:
+            return 0
+        #Recursively process the left and right subtree as this is a postorder DFS
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        #Choose the larger subtree depth plus the current node 
+        return 1 + max(left_depth, right_depth)
